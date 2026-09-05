a50fdf83bf56f365d3d4ce9b8702abaade30d838
Caio <caio@example.com>
2026-09-05 16:20:49 -0300

    Add goCRMAPISettings.php

---
+++ php/goCRMAPISettings.php
@@ -0,0 +1,6 @@
+<?php
+define ('gourl', 'https://dialddm.grupoddm.ia.br/goAPIv2');
+define ('goUser', 'goAPI');
+define ('goPass', 'KToB93bzjGd1RS4mDqePJ6Uk.jgNRrK');
+define ('responsetype', 'json');
+?>
