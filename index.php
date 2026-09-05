dca9b4d6d759fd16673536df3f4b523667ec6de6
Caio <caio@example.com>
2026-09-05 16:21:31 -0300

    Clean error reporting for production

--- index.php
+++ index.php
@@ -23,9 +23,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	error_reporting(E_ERROR | E_PARSE);
 
 	// check if Creamy has been installed.
 	require_once('./php/CRMDefaults.php');
--- login.php
+++ login.php
@@ -22,9 +22,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	error_reporting(E_ERROR | E_PARSE);
 
 	require_once('./php/CRMDefaults.php');
 	require_once('./php/UIHandler.php');
